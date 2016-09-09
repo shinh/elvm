cfd284420d72e0862158e087956a5ffb34b1fd78
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-09-10 07:28:38 +0900

    Have more state in Parser

--- ir/ir.c
+++ ir/ir.c
@@ -14,6 +14,12 @@ typedef struct {
   int col;
   FILE* fp;
   Table* symtab;
+  int in_text;
+  Inst* text;
+  int pc;
+  Data* data;
+  int mp;
+  bool prev_jmp;
 } Parser;
 
 static void error(Parser* p, const char* msg) {
@@ -99,27 +105,248 @@ static Data* add_data(Data* d, int* mp, int v) {
   return n;
 }
 
+static void parse_line(Parser* p, int c) {
+  char buf[32];
+  buf[0] = c;
+  read_while_ident(p, buf + 1, 30);
+  Op op = OP_UNSET;
+  if (!strcmp(buf, "mov")) {
+    op = MOV;
+  } else if (!strcmp(buf, "add")) {
+    op = ADD;
+  } else if (!strcmp(buf, "sub")) {
+    op = SUB;
+  } else if (!strcmp(buf, "load")) {
+    op = LOAD;
+  } else if (!strcmp(buf, "store")) {
+    op = STORE;
+  } else if (!strcmp(buf, "putc")) {
+    op = PUTC;
+  } else if (!strcmp(buf, "getc")) {
+    op = GETC;
+  } else if (!strcmp(buf, "exit")) {
+    op = EXIT;
+  } else if (!strcmp(buf, "dump")) {
+    op = DUMP;
+  } else if (!strcmp(buf, "jeq")) {
+    op = JEQ;
+  } else if (!strcmp(buf, "jne")) {
+    op = JNE;
+  } else if (!strcmp(buf, "jlt")) {
+    op = JLT;
+  } else if (!strcmp(buf, "jgt")) {
+    op = JGT;
+  } else if (!strcmp(buf, "jle")) {
+    op = JLE;
+  } else if (!strcmp(buf, "jge")) {
+    op = JGE;
+  } else if (!strcmp(buf, "jmp")) {
+    op = JMP;
+  } else if (!strcmp(buf, "eq")) {
+    op = EQ;
+  } else if (!strcmp(buf, "ne")) {
+    op = NE;
+  } else if (!strcmp(buf, "lt")) {
+    op = LT;
+  } else if (!strcmp(buf, "gt")) {
+    op = GT;
+  } else if (!strcmp(buf, "le")) {
+    op = LE;
+  } else if (!strcmp(buf, "ge")) {
+    op = GE;
+  } else if (!strcmp(buf, ".text")) {
+    p->in_text = 1;
+    return;
+  } else if (!strcmp(buf, ".data")) {
+    // TODO: Set op = DATA.
+    p->in_text = 0;
+    return;
+  } else if (!strcmp(buf, ".long")) {
+    if (p->in_text)
+      error(p, "in text");
+    skip_ws(p);
+
+    c = ir_getc(p);
+    if (!isdigit(c) && c != '-')
+      error(p, "number expected");
+    p->data = add_data(p->data, &p->mp, read_int(p, c));
+    // TODO: Set op = LONG.
+    return;
+  } else if (!strcmp(buf, ".string")) {
+    if (p->in_text)
+      error(p, "in text");
+    skip_ws(p);
+    if (ir_getc(p) != '"')
+      error(p, "expected open '\"'");
+
+    c = ir_getc(p);
+    while (c != '"') {
+      if (c == '\\') {
+        c = ir_getc(p);
+        if (c == 'n')
+          c = 10;
+        else
+          error(p, "unknown escape");
+        p->data = add_data(p->data, &p->mp, c);
+      } else {
+        p->data = add_data(p->data, &p->mp, c);
+      }
+      c = ir_getc(p);
+    }
+    p->data = add_data(p->data, &p->mp, 0);
+
+    return;
+  } else {
+    c = ir_getc(p);
+    if (c == ':') {
+      int value = 0;
+      if (p->in_text) {
+        if (!p->prev_jmp)
+          p->pc++;
+        value = p->pc;
+      } else {
+        value = p->mp;
+      }
+      p->symtab = TABLE_ADD(p->symtab, buf, value);
+      return;
+    }
+    error(p, "unknown op");
+  }
+
+  if (op < 0) {
+    error(p, "oops");
+  }
+  int argc;
+  if (op <= STORE)
+    argc = 2;
+  else if (op <= GETC)
+    argc = 1;
+  else if (op <= EXIT)
+    argc = 0;
+  else if (op <= JGE)
+    argc = 3;
+  else if (op <= JMP)
+    argc = 1;
+  else if (op <= GE)
+    argc = 2;
+  else if (op == DUMP)
+    argc = 0;
+  else
+    error(p, "oops");
+
+  p->text->next = calloc(1, sizeof(Inst));
+  p->text = p->text->next;
+  p->text->op = op;
+  p->text->pc = p->pc;
+  p->text->lineno = p->lineno;
+
+  Value args[3];
+  for (int i = 0; i < argc; i++) {
+    skip_ws(p);
+    if (i) {
+      c = ir_getc(p);
+      if (c != ',')
+        error(p, "comma expected");
+      skip_ws(p);
+    }
+
+    Value a;
+    c = ir_getc(p);
+    if (isdigit(c) || c == '-') {
+      a.type = IMM;
+      a.imm = read_int(p, c);
+    } else {
+      buf[0] = c;
+      read_while_ident(p, buf + 1, 30);
+      if (isupper(c)) {
+        a.type = REG;
+        if (!strcmp(buf, "A")) {
+          a.reg = A;
+        } else if (!strcmp(buf, "B")) {
+          a.reg = B;
+        } else if (!strcmp(buf, "C")) {
+          a.reg = C;
+        } else if (!strcmp(buf, "D")) {
+          a.reg = D;
+        } else if (!strcmp(buf, "SP")) {
+          a.reg = SP;
+        } else if (!strcmp(buf, "BP")) {
+          a.reg = BP;
+        } else {
+          error(p, "unknown reg");
+        }
+      } else {
+        a.type = TMP;
+        a.tmp = strdup(buf);
+      }
+    }
+    args[i] = a;
+  }
+
+  p->prev_jmp = false;
+  switch (op) {
+    case MOV:
+    case ADD:
+    case SUB:
+    case LOAD:
+    case STORE:
+    case EQ:
+    case NE:
+    case LT:
+    case GT:
+    case LE:
+    case GE:
+      p->text->src = args[1];
+    case GETC:
+      p->text->dst = args[0];
+      break;
+    case PUTC:
+      p->text->src = args[0];
+    case EXIT:
+    case DUMP:
+      break;
+    case JEQ:
+    case JNE:
+    case JLT:
+    case JGT:
+    case JLE:
+    case JGE:
+      p->text->dst = args[1];
+      p->text->src = args[2];
+    case JMP:
+      p->text->jmp = args[0];
+      p->pc++;
+      p->prev_jmp = true;
+      break;
+    default:
+      error(p, "oops");
+  }
+
+  //dump_inst(text);
+  //fprintf(stderr, "\n");
+}
+
 static Module* parse_eir(Parser* p) {
   Inst text_root = {};
-  Inst* text = &text_root;
   Data data_root = {};
-  Data* data = &data_root;
-  char buf[32];
-  int in_text = 1;
-  int pc = 0;
-  int mp = 0;
   int c;
-  bool prev_jmp = 1;
-  p->lineno = 1;
 
-  text->next = calloc(1, sizeof(Inst));
-  text = text->next;
-  text->op = JMP;
-  text->pc = pc++;
-  text->lineno = -1;
-  text->jmp.type = TMP;
-  text->jmp.tmp = "main";
-  text->next = 0;
+  p->in_text = 1;
+  p->lineno = 1;
+  p->text = &text_root;
+  p->data = &data_root;
+  p->pc = 0;
+  p->mp = 0;
+  p->prev_jmp = true;
+
+  p->text->next = calloc(1, sizeof(Inst));
+  p->text = p->text->next;
+  p->text->op = JMP;
+  p->text->pc = p->pc++;
+  p->text->lineno = -1;
+  p->text->jmp.type = TMP;
+  p->text->jmp.tmp = "main";
+  p->text->next = 0;
   p->symtab = TABLE_ADD(p->symtab, "main", 1);
 
   for (;;) {
@@ -131,227 +358,13 @@ static Module* parse_eir(Parser* p) {
     if (c == '#') {
       skip_until_ret(p);
     } else if (c == '_' || c == '.' || isalpha(c)) {
-      buf[0] = c;
-      read_while_ident(p, buf + 1, 30);
-      Op op = OP_UNSET;
-      if (!strcmp(buf, "mov")) {
-        op = MOV;
-      } else if (!strcmp(buf, "add")) {
-        op = ADD;
-      } else if (!strcmp(buf, "sub")) {
-        op = SUB;
-      } else if (!strcmp(buf, "load")) {
-        op = LOAD;
-      } else if (!strcmp(buf, "store")) {
-        op = STORE;
-      } else if (!strcmp(buf, "putc")) {
-        op = PUTC;
-      } else if (!strcmp(buf, "getc")) {
-        op = GETC;
-      } else if (!strcmp(buf, "exit")) {
-        op = EXIT;
-      } else if (!strcmp(buf, "dump")) {
-        op = DUMP;
-      } else if (!strcmp(buf, "jeq")) {
-        op = JEQ;
-      } else if (!strcmp(buf, "jne")) {
-        op = JNE;
-      } else if (!strcmp(buf, "jlt")) {
-        op = JLT;
-      } else if (!strcmp(buf, "jgt")) {
-        op = JGT;
-      } else if (!strcmp(buf, "jle")) {
-        op = JLE;
-      } else if (!strcmp(buf, "jge")) {
-        op = JGE;
-      } else if (!strcmp(buf, "jmp")) {
-        op = JMP;
-      } else if (!strcmp(buf, "eq")) {
-        op = EQ;
-      } else if (!strcmp(buf, "ne")) {
-        op = NE;
-      } else if (!strcmp(buf, "lt")) {
-        op = LT;
-      } else if (!strcmp(buf, "gt")) {
-        op = GT;
-      } else if (!strcmp(buf, "le")) {
-        op = LE;
-      } else if (!strcmp(buf, "ge")) {
-        op = GE;
-      } else if (!strcmp(buf, ".text")) {
-        in_text = 1;
-        continue;
-      } else if (!strcmp(buf, ".data")) {
-        in_text = 0;
-        continue;
-      } else if (!strcmp(buf, ".long")) {
-        if (in_text)
-          error(p, "in text");
-        skip_ws(p);
-
-        c = ir_getc(p);
-        if (!isdigit(c) && c != '-')
-          error(p, "number expected");
-        data = add_data(data, &mp, read_int(p, c));
-        continue;
-      } else if (!strcmp(buf, ".string")) {
-        if (in_text)
-          error(p, "in text");
-        skip_ws(p);
-        if (ir_getc(p) != '"')
-          error(p, "expected open '\"'");
-
-        c = ir_getc(p);
-        while (c != '"') {
-          if (c == '\\') {
-            c = ir_getc(p);
-            if (c == 'n')
-              c = 10;
-            else
-              error(p, "unknown escape");
-            data = add_data(data, &mp, c);
-          } else {
-            data = add_data(data, &mp, c);
-          }
-          c = ir_getc(p);
-        }
-        data = add_data(data, &mp, 0);
-
-        continue;
-      } else {
-        c = ir_getc(p);
-        if (c == ':') {
-          int value = 0;
-          if (in_text) {
-            if (!prev_jmp)
-              pc++;
-            value = pc;
-          } else {
-            value = mp;
-          }
-          p->symtab = TABLE_ADD(p->symtab, buf, value);
-          continue;
-        }
-        error(p, "unknown op");
-      }
-
-      if (op < 0) {
-        error(p, "oops");
-      }
-      int argc;
-      if (op <= STORE)
-        argc = 2;
-      else if (op <= GETC)
-        argc = 1;
-      else if (op <= EXIT)
-        argc = 0;
-      else if (op <= JGE)
-        argc = 3;
-      else if (op <= JMP)
-        argc = 1;
-      else if (op <= GE)
-        argc = 2;
-      else if (op == DUMP)
-        argc = 0;
-      else
-        error(p, "oops");
-
-      text->next = calloc(1, sizeof(Inst));
-      text = text->next;
-      text->op = op;
-      text->pc = pc;
-      text->lineno = p->lineno;
-
-      Value args[3];
-      for (int i = 0; i < argc; i++) {
-        skip_ws(p);
-        if (i) {
-          c = ir_getc(p);
-          if (c != ',')
-            error(p, "comma expected");
-          skip_ws(p);
-        }
-
-        Value a;
-        c = ir_getc(p);
-        if (isdigit(c) || c == '-') {
-          a.type = IMM;
-          a.imm = read_int(p, c);
-        } else {
-          buf[0] = c;
-          read_while_ident(p, buf + 1, 30);
-          if (isupper(c)) {
-            a.type = REG;
-            if (!strcmp(buf, "A")) {
-              a.reg = A;
-            } else if (!strcmp(buf, "B")) {
-              a.reg = B;
-            } else if (!strcmp(buf, "C")) {
-              a.reg = C;
-            } else if (!strcmp(buf, "D")) {
-              a.reg = D;
-            } else if (!strcmp(buf, "SP")) {
-              a.reg = SP;
-            } else if (!strcmp(buf, "BP")) {
-              a.reg = BP;
-            } else {
-              error(p, "unknown reg");
-            }
-          } else {
-            a.type = TMP;
-            a.tmp = strdup(buf);
-          }
-        }
-        args[i] = a;
-      }
-
-      prev_jmp = false;
-      switch (op) {
-        case MOV:
-        case ADD:
-        case SUB:
-        case LOAD:
-        case STORE:
-        case EQ:
-        case NE:
-        case LT:
-        case GT:
-        case LE:
-        case GE:
-          text->src = args[1];
-        case GETC:
-          text->dst = args[0];
-          break;
-        case PUTC:
-          text->src = args[0];
-        case EXIT:
-        case DUMP:
-          break;
-        case JEQ:
-        case JNE:
-        case JLT:
-        case JGT:
-        case JLE:
-        case JGE:
-          text->dst = args[1];
-          text->src = args[2];
-        case JMP:
-          text->jmp = args[0];
-          pc++;
-          prev_jmp = true;
-          break;
-        default:
-          error(p, "oops");
-      }
-
-      //dump_inst(text);
-      //fprintf(stderr, "\n");
+      parse_line(p, c);
     } else {
       error(p, "unexpected char");
     }
   }
 
-  p->symtab = TABLE_ADD(p->symtab, "_edata", mp);
+  p->symtab = TABLE_ADD(p->symtab, "_edata", p->mp);
 
   Module* m = malloc(sizeof(Module));
   m->text = text_root.next;
