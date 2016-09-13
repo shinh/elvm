08604cd638ba4ee0cff9f0f29b883badd8a2766c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-09-13 21:19:34 +0900

    Fix target.mk for multi targets

--- target.mk
+++ target.mk
@@ -1,7 +1,7 @@
 include clear_vars.mk
 SRCS := $(OUT.eir)
 EXT := $(TARGET)
-CMD = $(ELC) -$(TARGET) $2 > $1.tmp && mv $1.tmp $1
+$(eval CMD = $$(ELC) -$(TARGET) $$2 > $$1.tmp && chmod 755 $$1.tmp && mv $$1.tmp $$1)
 OUT.eir.$(TARGET) := $(SRCS:%=%.$(EXT))
 include build.mk
 
@@ -9,7 +9,7 @@ include clear_vars.mk
 SRCS := $(OUT.eir.$(TARGET))
 EXT := out
 DEPS := $(TEST_INS) runtest.sh
-CMD = ./runtest.sh $1 $(RUNNER) $2
+$(eval CMD = ./runtest.sh $$1 $(RUNNER) $$2)
 OUT.eir.$(TARGET).out := $(SRCS:%=%.$(EXT))
 include build.mk
 
