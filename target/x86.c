e6b69fb3730d871e6141d9a26f2530b0361c9d56
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-09-14 03:46:02 +0900

    Use EDI to represent SP
    ESP is necessary to keep the real stack.

--- target/x86.c
+++ target/x86.c
@@ -6,11 +6,22 @@
 #include <target/util.h>
 
 static int REGNO[] = {
-  0, 3, 1, 2, 5, 4, 6, 7
+  0,  // A
+  3,  // B
+  1,  // C
+  2,  // D
+  5,  // BP
+  7,  // SP - EDI
+  6,  // ESI
+  4   // ESP
 };
 
+#define EDI SP
 #define ESI ((Reg)6)
-#define EDI ((Reg)7)
+#define ESP ((Reg)7)
+
+static const int TEXT_START = 0x100000;
+static const int HEADER_SIZE = 84;
 
 static void emit_header(uint32_t filesz) {
   Elf32_Ehdr ehdr = {
@@ -20,7 +31,7 @@ static void emit_header(uint32_t filesz) {
     .e_type = 2,
     .e_machine = 3,
     .e_version = 1,
-    .e_entry = 0x100000 + 84,
+    .e_entry = TEXT_START + HEADER_SIZE,
     .e_phoff = 52,
     .e_shoff = 0,
     .e_flags = 0,
@@ -34,10 +45,10 @@ static void emit_header(uint32_t filesz) {
   Elf32_Phdr phdr = {
     .p_type = 1,
     .p_offset = 0,
-    .p_vaddr = 0x100000,
-    .p_paddr = 0x100000,
-    .p_filesz = filesz + 84,
-    .p_memsz = filesz + 84,
+    .p_vaddr = TEXT_START,
+    .p_paddr = TEXT_START,
+    .p_filesz = filesz + HEADER_SIZE,
+    .p_memsz = filesz + HEADER_SIZE,
     .p_flags = 5,
     .p_align = 0x1000
   };
@@ -88,14 +99,15 @@ static void emit_cmp(Inst* inst) {
   }
 }
 
-static void emit_jcc(Inst* inst, int op, int* pc2addr) {
+static void emit_jcc(Inst* inst, int op, int* pc2addr, int rodata_addr) {
   if (op) {
     emit_cmp(inst);
-    emit_2(op, inst->jmp.type == REG ? 2 : 5);
+    emit_2(op, inst->jmp.type == REG ? 7 : 5);
   }
 
   if (inst->jmp.type == REG) {
-    emit_2(0xff, 0xe0 | REGNO[inst->jmp.reg]);
+    emit_3(0xff, 0x24, 0x85 | (REGNO[inst->jmp.reg] << 3));
+    emit_le(rodata_addr);
   } else {
     emit_1(0xe9);
     emit_le(pc2addr[inst->jmp.imm] - emit_cnt() - 4);
@@ -104,7 +116,7 @@ static void emit_jcc(Inst* inst, int op, int* pc2addr) {
 
 static void init_state(Data* data) {
   emit_mov_imm(B, 0);
-  emit_mov_imm(C, 1 << 24);
+  emit_mov_imm(C, (1 << 24) * 4);
   emit_mov_imm(D, 3);  // PROT_READ | PROT_WRITE
   emit_mov_imm(ESI, 0x22);  // MAP_PRIVATE | MAP_ANONYMOUS
   emit_mov_imm(EDI, -1);
@@ -123,8 +135,7 @@ static void init_state(Data* data) {
     }
   }
 
-  // lea ESP, [EAX + (1<<24)]
-  emit_6(0x8d, 0xa0, 0, 0, 0, 1);
+  emit_mov_imm(SP, 1 << 24);
   // xor EAX, EAX
   emit_2(0x31, 0xc0);
   // xor EBX, EBX
@@ -137,7 +148,7 @@ static void init_state(Data* data) {
   emit_2(0x31, 0xed);
 }
 
-static void emit_inst(Inst* inst, int* pc2addr) {
+static void emit_inst(Inst* inst, int* pc2addr, int rodata_addr) {
   switch (inst->op) {
     case MOV:
       emit_mov(inst->dst.reg, &inst->src);
@@ -175,7 +186,8 @@ static void emit_inst(Inst* inst, int* pc2addr) {
       emit_1(0x89);
     store_load_common:
       if (inst->src.type == REG) {
-        emit_2(4 | (REGNO[inst->dst.reg] << 3), 0x30 | REGNO[inst->src.reg]);
+        emit_2(4 | (REGNO[inst->dst.reg] << 3),
+               0x86 | (REGNO[inst->src.reg] << 3));
       } else {
         emit_1(0x86 | (REGNO[inst->dst.reg] << 3));
         emit_le(inst->src.imm);
@@ -183,23 +195,29 @@ static void emit_inst(Inst* inst, int* pc2addr) {
       break;
 
     case PUTC:
+      // push EDI
+      emit_1(0x57);
       emit_mov(EDI, &inst->src);
       // push EAX, ECX, EDX, EBX, EDI
       emit_5(0x50, 0x51, 0x52, 0x53, 0x57);
       emit_mov_imm(B, 1);  // stdout
-      emit_mov_reg(C, SP);
+      emit_mov_reg(C, ESP);
       emit_mov_imm(D, 1);
       emit_mov_imm(A, 4);  // write
       emit_int80();
       // pop EDI, EBX, EDX, ECX, EAX
       emit_5(0x5f, 0x5b, 0x5a, 0x59, 0x58);
+      // pop EDI
+      emit_1(0x5f);
       break;
 
     case GETC:
+      // push EDI
+      emit_1(0x57);
       // push EAX, ECX, EDX, EBX, EDI
       emit_5(0x50, 0x51, 0x52, 0x53, 0x57);
       emit_mov_imm(B, 0);  // stdin
-      emit_mov_reg(C, SP);
+      emit_mov_reg(C, ESP);
       emit_mov_imm(D, 1);
       emit_mov_imm(A, 3);  // read
       emit_int80();
@@ -215,6 +233,8 @@ static void emit_inst(Inst* inst, int* pc2addr) {
       // pop EBX, EDX, ECX, EAX
       emit_4(0x5b, 0x5a, 0x59, 0x58);
       emit_mov_reg(inst->src.reg, EDI);
+      // pop EDI
+      emit_1(0x5f);
       break;
 
     case EXIT:
@@ -263,31 +283,31 @@ static void emit_inst(Inst* inst, int* pc2addr) {
       break;
 
     case JEQ:
-      emit_jcc(inst, 0x75, pc2addr);
+      emit_jcc(inst, 0x75, pc2addr, rodata_addr);
       break;
 
     case JNE:
-      emit_jcc(inst, 0x74, pc2addr);
+      emit_jcc(inst, 0x74, pc2addr, rodata_addr);
       break;
 
     case JLT:
-      emit_jcc(inst, 0x7d, pc2addr);
+      emit_jcc(inst, 0x7d, pc2addr, rodata_addr);
       break;
 
     case JGT:
-      emit_jcc(inst, 0x7e, pc2addr);
+      emit_jcc(inst, 0x7e, pc2addr, rodata_addr);
       break;
 
     case JLE:
-      emit_jcc(inst, 0x7f, pc2addr);
+      emit_jcc(inst, 0x7f, pc2addr, rodata_addr);
       break;
 
     case JGE:
-      emit_jcc(inst, 0x7c, pc2addr);
+      emit_jcc(inst, 0x7c, pc2addr, rodata_addr);
       break;
 
     case JMP:
-      emit_jcc(inst, 0, pc2addr);
+      emit_jcc(inst, 0, pc2addr, rodata_addr);
       break;
 
     default:
@@ -311,16 +331,22 @@ void target_x86(Module* module) {
       pc2addr[inst->pc] = emit_cnt();
     }
     prev_pc = inst->pc;
-    emit_inst(inst, pc2addr);
+    emit_inst(inst, pc2addr, 0);
   }
 
-  emit_header(emit_cnt());
+  int rodata_addr = TEXT_START + emit_cnt() + HEADER_SIZE;
+
+  emit_header(emit_cnt() + pc_cnt * sizeof(int));
 
   emit_reset();
   emit_start();
   init_state(module->data);
 
   for (Inst* inst = module->text; inst; inst = inst->next) {
-    emit_inst(inst, pc2addr);
+    emit_inst(inst, pc2addr, rodata_addr);
+  }
+
+  for (int i = 0; i < pc_cnt; i++) {
+    emit_le(TEXT_START + pc2addr[i] + HEADER_SIZE);
   }
 }
