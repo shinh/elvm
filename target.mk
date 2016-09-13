58b35d4ce445db931a35cb5439df608433abca72
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-09-13 20:54:22 +0900

    Test backends

--- target.mk
+++ target.mk
@@ -10,4 +10,10 @@ SRCS := $(OUT.eir.$(TARGET))
 EXT := out
 DEPS := $(TEST_INS) runtest.sh
 CMD = ./runtest.sh $1 $(RUNNER) $2
+OUT.eir.$(TARGET).out := $(SRCS:%=%.$(EXT))
 include build.mk
+
+include clear_vars.mk
+EXPECT := eir.out
+ACTUAL := eir.$(TARGET).out
+include diff.mk
