767e5450bf04ef9eeb50f2417a8f1cccc38d1827
hamaji <shinichiro.hamaji@gmail.com>
2016-09-20 19:42:23 +0900

    Run EIR on Web UI

--- tools/makeweb.rb
+++ tools/makeweb.rb
@@ -3,7 +3,7 @@
 require 'fileutils'
 require 'json'
 
-system("make out/8cc.c.eir.js out/elc.c.eir.js")
+system("make out/8cc.c.eir.js out/elc.c.eir.js out/eli.c.eir.js")
 
 headers = {}
 Dir.glob('libc/*.h').each do |hf|
@@ -16,6 +16,7 @@ FileUtils.mkdir_p('web')
 FileUtils.ln_sf('../tools/8cc.js.html', 'web')
 FileUtils.ln_sf('../out/8cc.c.eir.js', 'web')
 FileUtils.ln_sf('../out/elc.c.eir.js', 'web')
+FileUtils.ln_sf('../out/eli.c.eir.js', 'web')
 File.open('web/headers.js', 'w') do |of|
   of.print 'var HEADERS = '
   of.print JSON.dump(headers)
