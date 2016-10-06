ae0f8fc2a228d57988a24462e71f74025808d94e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-10-06 13:13:33 +0900

    Do not convert piet code

--- tools/runpiet.sh
+++ tools/runpiet.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-convert $1 $1.png
-
-(cat /dev/stdin && /bin/echo -ne "\0") | npiet/npiet -q $1.png
+#convert $1 $1.png
+(cat /dev/stdin && /bin/echo -ne "\0") | npiet/npiet -q $1
 exit
 
 # http://www.matthias-ernst.eu/pietcompiler.html
