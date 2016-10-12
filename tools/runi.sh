e768ab0d2fb2c4fcc64bb9c63a1b198c3ba9d734
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-10-13 05:58:20 +0900

    Find ick's gcc flags automatically

--- tools/runi.sh
+++ tools/runi.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+if [ ! -e out/ick.flags ]; then
+    orig_dir=$(pwd)
+    dir=$(mktemp -d)
+    cd $dir
+
+    echo 'DO GIVE UP' > do_nothing.i
+    ick -Y do_nothing.i > do_nothing.out 2>&1
+
+    echo -n 'ICK_CFLAGS=' > ick.flags
+    perl -ple 's/.*?(( -I[^ ]+)+).*/"\1"/' do_nothing.out >> ick.flags
+    echo -n 'ICK_LDFLAGS=' >> ick.flags
+    perl -ple 's/.*?(( -L[^ ]+)+).*/"\1"/' do_nothing.out >> ick.flags
+    echo -n 'ICK_LIBS=' >> ick.flags
+    perl -ple 's/.*?(( -l[^ ]+)+).*/"\1"/' do_nothing.out >> ick.flags
+
+    mv ick.flags ${orig_dir}/out
+    cd $orig_dir
+    rm -fr $dir
+fi
+
+. out/ick.flags
+
 ick -b -o $1 > $1.c
-tinycc/tcc -Btinycc -I/usr/include/ick-0.29 $1.c -lick -o $1.c.exe
+tinycc/tcc -Btinycc ${ICK_CFLAGS} ${ICK_LDFLAGS} $1.c ${ICK_LIBS} -o $1.c.exe
 $1.c.exe
