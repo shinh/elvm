6e42a12322e0ecd69dbfebd3178658dc4bea277d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-10-21 05:42:22 +0900

    Make it possible to run vim tests in parallel
    by using unique temporary file names.

--- tools/runvim.sh
+++ tools/runvim.sh
@@ -2,8 +2,10 @@
 
 # Runs Vim script code by Ex mode with silent mode.
 # Only readfile() can treat binary input (e.g. input without EOL at the end of file) properly in Vim.
-# This script saves stdin to /tmp/out and outputs the result (stdout) to /tmp/out after :source the script.
+# This script saves stdin to /tmp/*.out and outputs the result (stdout) to /tmp/*.out after :source the script.
 
-cat > /tmp/out
-TERM=dumb vim -X -N -b -n -u NONE -i NONE -V1 -e -s --cmd "call setline(1, readfile('/tmp/out', 'b')) | set noeol | source $1 | w! /tmp/out | qa!" > /dev/null 2>&1
-cat /tmp/out
+out=/tmp/$(basename $1).out
+
+cat > ${out}
+TERM=dumb vim -X -N -b -n -u NONE -i NONE -V1 -e -s --cmd "call setline(1, readfile('${out}', 'b')) | set noeol | source $1 | w! ${out} | qa!" > /dev/null 2>&1
+cat ${out}
