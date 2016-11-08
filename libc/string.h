fa8bdcb372b5894e4c095c0b4e3abcea8cc34a0e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-11-08 20:25:17 +0900

    Add *_MAX and strpbrk

--- libc/string.h
+++ libc/string.h
@@ -189,4 +189,19 @@ strncasecmp(const char *s1, const char *s2, size_t n)
   return (0);
 }
 
+// From Bionic:
+char *
+strpbrk(const char *s1, const char *s2)
+{
+	const char *scanp;
+	int c, sc;
+
+	while ((c = *s1++) != 0) {
+		for (scanp = s2; (sc = *scanp++) != 0;)
+			if (sc == c)
+				return ((char *)(s1 - 1));
+	}
+	return (NULL);
+}
+
 #endif  // ELVM_LIBC_STRING_H_
