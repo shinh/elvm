f700a2d2e2401c0de58a2fcba42e98a35b8dd3db
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-11-09 21:32:34 +0900

    Optimize bit operations

--- libc/_builtin.h
+++ libc/_builtin.h
@@ -84,93 +84,84 @@ static unsigned int __builtin_mod(unsigned int a, unsigned int b) {
   return r.rem;
 }
 
-static void __builtin_to_bitarray(unsigned int v, unsigned char* b) {
-  static const int bits_table[] = {
-    0x800000, 0x400000, 0x200000, 0x100000,
-    0x80000, 0x40000, 0x20000, 0x10000,
-    0x8000, 0x4000, 0x2000, 0x1000,
-    0x800, 0x400, 0x200, 0x100,
-    0x80, 0x40, 0x20, 0x10,
-    0x8, 0x4, 0x2, 0x1,
-  };
-  for (int i = 0; i < 24; i++) {
-    int t = bits_table[i];
-    if (v >= t) {
-      b[i] = 1;
-      v -= t;
-    } else {
-      b[i] = 0;
-    }
-  }
-}
+static const int __builtin_bits_table[] = {
+  0x800000, 0x400000, 0x200000, 0x100000,
+  0x80000, 0x40000, 0x20000, 0x10000,
+  0x8000, 0x4000, 0x2000, 0x1000,
+  0x800, 0x400, 0x200, 0x100,
+  0x80, 0x40, 0x20, 0x10,
+  0x8, 0x4, 0x2, 0x1,
+};
 
-static unsigned int __builtin_from_bitarray(unsigned char* b) {
-  unsigned int v = 0;
-  for (int i = 0; i < 24; i++) {
-    v += v;
-    v += b[i];
-  }
-  return v;
-}
+#define __BUILTIN_TO_BIT(v, t) (v >= t ? (v -= t, 1) : 0)
 
 static unsigned int __builtin_and(unsigned int a, unsigned int b) {
-  unsigned char av[24];
-  unsigned char bv[24];
-  __builtin_to_bitarray(a, av);
-  __builtin_to_bitarray(b, bv);
+  int r = 0;
   for (int i = 0; i < 24; i++) {
-    av[i] = av[i] && bv[i];
+    int t = __builtin_bits_table[i];
+    int a1 = __BUILTIN_TO_BIT(a, t);
+    int b1 = __BUILTIN_TO_BIT(b, t);
+    if (a1 && b1)
+      r += t;
   }
-  return __builtin_from_bitarray(av);
+  return r;
 }
 
 static unsigned int __builtin_or(unsigned int a, unsigned int b) {
-  unsigned char av[24];
-  unsigned char bv[24];
-  __builtin_to_bitarray(a, av);
-  __builtin_to_bitarray(b, bv);
+  int r = 0;
   for (int i = 0; i < 24; i++) {
-    av[i] = av[i] || bv[i];
+    int t = __builtin_bits_table[i];
+    int a1 = __BUILTIN_TO_BIT(a, t);
+    int b1 = __BUILTIN_TO_BIT(b, t);
+    if (a1 || b1)
+      r += t;
   }
-  return __builtin_from_bitarray(av);
+  return r;
 }
 
 static unsigned int __builtin_xor(unsigned int a, unsigned int b) {
-  unsigned char av[24];
-  unsigned char bv[24];
-  __builtin_to_bitarray(a, av);
-  __builtin_to_bitarray(b, bv);
+  int r = 0;
   for (int i = 0; i < 24; i++) {
-    av[i] = av[i] != bv[i];
+    int t = __builtin_bits_table[i];
+    int a1 = __BUILTIN_TO_BIT(a, t);
+    int b1 = __BUILTIN_TO_BIT(b, t);
+    if (a1 != b1)
+      r += t;
   }
-  return __builtin_from_bitarray(av);
+  return r;
 }
 
 static unsigned int __builtin_not(unsigned int a) {
-  unsigned char av[24];
-  __builtin_to_bitarray(a, av);
+  int r = 0;
   for (int i = 0; i < 24; i++) {
-    av[i] = !av[i];
+    int t = __builtin_bits_table[i];
+    int a1 = __BUILTIN_TO_BIT(a, t);
+    if (!a1)
+      r += t;
   }
-  return __builtin_from_bitarray(av);
+  return r;
 }
 
 static unsigned int __builtin_shl(unsigned int a, unsigned int b) {
-  unsigned char av[48];
-  __builtin_to_bitarray(a, av);
-  for (int i = 0; i < b; i++) {
-    av[24+i] = 0;
+  int r = 0;
+  for (int i = b; i < 24; i++) {
+    int t = __builtin_bits_table[i];
+    int a1 = __BUILTIN_TO_BIT(a, t);
+    if (a1)
+      r += __builtin_bits_table[i-b];
   }
-  return __builtin_from_bitarray(av + b);
+  return r;
 }
 
 static unsigned int __builtin_shr(unsigned int a, unsigned int b) {
-  unsigned char av[48];
-  __builtin_to_bitarray(a, av + 24);
-  for (int i = 0; i < b; i++) {
-    av[24-i-1] = 0;
+  int r = 0;
+  for (int i = b; i < 24; i++) {
+    int t = __builtin_bits_table[i-b];
+    int a1 = __BUILTIN_TO_BIT(a, t);
+    if (a1)
+      r += __builtin_bits_table[i];
   }
-  return __builtin_from_bitarray(av + 24 - b);
+  return r;
 }
 
 #endif  // ELVM_LIBC_BUILTIN_H_
