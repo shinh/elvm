5b86c1c78e0d38062efdde3d2762f68a6e7e04ef
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-11-09 12:17:58 +0900

    Correct the definition of stdbool.h

--- libc/stdbool.h
+++ libc/stdbool.h
@@ -1,8 +1,9 @@
 #ifndef ELVM_LIBC_STDBOOL_H_
 #define ELVM_LIBC_STDBOOL_H_
 
-typedef int bool;
+#define bool _Bool
 #define true 1
 #define false 0
+#define __bool_true_false_are_defined 1
 
 #endif  // ELVM_LIBC_STDBOOL_H_
