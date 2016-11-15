7b37b9006d88529a24c28d2bb71bd2d87ad3ded6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-11-15 13:53:14 -0800

    Implement putc for sed

--- target/sed.c
+++ target/sed.c
@@ -7,10 +7,11 @@
 
 static void sed_init_state(Data* data) {
   for (int i = 0; i < 6; i++) {
-    emit_line("s/$/%s=0\\n/", reg_names[i]);
+    emit_line("s/$/%s=0 /", reg_names[i]);
   }
+  emit_line("s/$/o= /");
   for (int mp = 0; data; data = data->next, mp++) {
-    emit_line("s/$/m%x=%x\\n/", mp, data->v);
+    emit_line("s/$/m%x=%x /", mp, data->v);
   }
   emit_line("x");
 }
@@ -46,8 +47,12 @@ static void sed_emit_inst(Inst* inst) {
 
   case PUTC:
     sed_emit_src(inst);
-    emit_line("s/.*\(..\)$/\1/");
-    emit_line("s/^0*//");
+    emit_line("s/^/0/");
+    emit_line("s/.*\\(..\\)$/\\1/");
+    emit_line("G");
+    emit_line("s/^\\(..\\)\\n\\(.*o=[^ ]*\\)/\\2\\1/");
+    emit_line("x");
+    emit_line("s/.*//");
     break;
 
   case GETC: {
@@ -55,6 +60,7 @@ static void sed_emit_inst(Inst* inst) {
   }
 
   case EXIT:
+    emit_line("bexit");
     break;
 
   case DUMP:
@@ -95,4 +101,33 @@ void target_sed(Module* module) {
     prev_pc = inst->pc;
     sed_emit_inst(inst);
   }
+
+  emit_line(":exit");
+  emit_line("s/.*//");
+  emit_line("x");
+  emit_line("s/.*o=\\([^ ]*\\).*/\\1/");
+  emit_line(":out_loop");
+  emit_line("/^$/bout_done");
+  for (int i = 0; i < 256; i++) {
+    printf("/^%x%x/{s/..//\nx\n", i / 16, i % 16);
+    if (i == 10) {
+      emit_line("p\ns/.*//\nx\n}");
+    } else {
+      char buf[6];
+      buf[0] = i;
+      buf[1] = 0;
+      if (i == '/' || i == '\\' || i == '&') {
+        buf[0] = '\\';
+        buf[1] = i;
+        buf[2] = 0;
+      } else if (i == 0 || (i >= 0xc2 && i <= 0xfd)) {
+        sprintf(buf, "\\x%x", i);
+      }
+      emit_line("s/$/%s/\nx\n}", buf);
+    }
+  }
+  emit_line("bout_loop");
+  emit_line(":out_done");
+  emit_line("x");
+  emit_line("p");
 }
