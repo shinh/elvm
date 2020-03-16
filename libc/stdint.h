e3ca3a0bf5508621fadebdbe3a39bc48754ea19b
jsc <jsc+bones@demigods.org>
2020-03-16 15:59:03 -0700

    Make a cast instead of relying on preprocessor directive.

--- libc/stdint.h
+++ libc/stdint.h
@@ -18,12 +18,12 @@ typedef long time_t;
 #define UINT8_MAX	(INT8_MAX * 2 + 1)
 #define INT16_MAX	0x7fff
 #define INT16_MIN	(-INT16_MAX - 1)
-#define UINT16_MAX	(__CONCAT(INT16_MAX, U) * 2U + 1U)
+#define UINT16_MAX	(((unsigned)INT16_MAX) * 2U + 1U)
 #define INT32_MAX	0x7fffffffL
 #define INT32_MIN	(-INT32_MAX - 1L)
-#define UINT32_MAX	(__CONCAT(INT32_MAX, U) * 2UL + 1UL)
+#define UINT32_MAX	(((unsigned)INT32_MAX) * 2UL + 1UL)
 #define INT64_MAX	0x7fffffffffffffffLL
 #define INT64_MIN	(-INT64_MAX - 1LL)
-#define UINT64_MAX	(__CONCAT(INT64_MAX, U) * 2ULL + 1ULL)
+#define UINT64_MAX	(((unsigned)INT64_MAX) * 2ULL + 1ULL)
 
 #endif  // ELVM_LIBC_STDINT_H_
